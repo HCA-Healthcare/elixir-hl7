08d15f66807ac3620a4b2d35d3aaab198f70f048
Bryan Hunter <bryan.hunter@hcahealthcare.com>
2018-05-23 08:50:22 -0500

    Formatted test

--- test/hl7_test.exs
+++ test/hl7_test.exs
@@ -4,7 +4,13 @@ defmodule Hl7Test do
 
   test "Wikipedia message roundtrip" do
     hl7 = Hl7.Examples.wikipedia_sample_hl7()
-    roundtrip = hl7 |> Hl7.Message.new() |> Hl7.Message.make_structs() |> to_string()
+
+    roundtrip =
+      hl7
+      |> Hl7.Message.new()
+      |> Hl7.Message.make_structs()
+      |> to_string()
+
     assert roundtrip == hl7
   end
 end
