cfdcc5e440f8e3d01b0d1d764eb9ccda0fbe5787
darkmarmot <scott.southworth@gmail.com>
2019-03-14 12:55:51 -0500

    test coverage at 98+%, good enough for now, removed SMAT stream as it doesn't look very standard

--- lib/hl7.ex
+++ lib/hl7.ex
@@ -6,17 +6,7 @@ defmodule HL7 do
 
   @buffer_size 32768
 
-  @type file_type_hl7 :: :mllp | :smat | :line | nil
-  @type prefix :: String.t() | Regex.t() | nil
-  @type suffix :: String.t() | Regex.t()
-
-  # ^K - VT (Vertical Tab) - 0x0B
-  @sb "\v"
-  # ^\ - FS (File Separator)
-  @eb <<0x1C>>
-  # ^M - CR (Carriage Return) - 0x0D
-  @cr "\r"
-  @ending @eb <> @cr
+  @type file_type_hl7 :: :mllp | :line | nil
 
   @doc """
   Opens an HL7 file stream of either `:mllp`, `:smat` or `:line`. If the file_type is not specified
@@ -48,26 +38,11 @@ defmodule HL7 do
       {:ok, :mllp} ->
         file_path
         |> File.stream!([], @buffer_size)
-        |> HL7.SplitStream.raw_to_messages(@sb, @ending)
-#        |> HL7.MLLPStream.raw_to_messages()
-      {:ok, :smat} ->
-        file_path
-        |> File.stream!([], @buffer_size)
-        |> HL7.SMATStream.raw_to_messages()
+        |> HL7.MLLPStream.raw_to_messages()
 
       {:error, reason} ->
         {:error, reason}
     end
-  end
-
-  @doc """
-  Opens an HL7 file stream with the given prefix and suffix strings used as message delimiters.
-  """
-  @spec open_hl7_file_stream(String.t(), prefix(), suffix()) :: Enumerable.t()
-  def open_hl7_file_stream(file_path, prefix, suffix) do
-    file_path
-    |> File.stream!([], 32768)
-    |> HL7.SplitStream.raw_to_messages(prefix, suffix)
   end
 
   @spec infer_file_type(String.t()) :: {:ok, :line} | {:ok, :mllp} | {:error, atom()}
--- lib/hl7/mllp_stream.ex
+++ lib/hl7/mllp_stream.ex
@@ -1,72 +1,68 @@
-#defmodule HL7.MLLPStream do
-#  @moduledoc false
-#  require Logger
-#
-#  # ^K - VT (Vertical Tab) - 0x0B
-#  @sb "\v"
-#  # ^\ - FS (File Separator)
-#  @eb <<0x1C>>
-#  # ^M - CR (Carriage Return) - 0x0D
-#  @cr "\r"
-#  @ending @eb <> @cr
-#
-#  @spec get_prefix() :: String.t()
-#  def get_prefix() do
-#    @sb
-#  end
-#
-#  @spec get_suffix() :: String.t()
-#  def get_suffix() do
-#    @ending
-#  end
-#
-#  @spec raw_to_messages(Enumerable.t()) :: Enumerable.t()
-#  def raw_to_messages(input_stream) do
-#    Stream.chunk_while(input_stream, "", &chunker/2, &after_chunking/1) |> Stream.concat()
-#  end
-#
-#  @spec after_sb(String.t()) :: nil | String.t()
-#  defp after_sb(text) do
-#    chunks = text |> String.split(@sb, parts: 2)
-#
-#    case chunks do
-#      [_chunk] -> nil
-#      [_sb, msg] -> msg
-#    end
-#  end
-#
-#  @spec to_list_and_remnant(list()) :: {:cont, list(), String.t()}
-#  defp to_list_and_remnant(potential_messages) do
-#    [remnant | reverse_msgs] = potential_messages |> Enum.reverse()
-#
-#    msgs =
-#      reverse_msgs
-#      |> Enum.map(&after_sb(&1))
-#      |> Enum.filter(fn m -> is_binary(m) and m != "" end)
-#      |> Enum.reverse()
-#
-#    {:cont, msgs, remnant}
-#  end
-#
-#  @spec chunker(String.t(), String.t()) :: {:cont, String.t()} | {:cont, list(), String.t()}
-#  defp chunker(element, acc) when is_binary(element) do
-#    # {:cont, chunk, acc} | {:cont, acc} | {:halt, acc})
-#
-#    text = acc <> element
-#    potential_msg_list = String.split(text, @ending)
-#
-#    case potential_msg_list do
-#      [not_found] -> {:cont, not_found}
-#      _ -> potential_msg_list |> to_list_and_remnant
-#    end
-#  end
-#
-#  defp chunker(_element, _acc) do
-#    raise(ArgumentError, message: "all elements in an MLLP stream must be binary")
-#  end
-#
-#  @spec after_chunking(any()) :: {:cont, list()}
-#  defp after_chunking(_acc) do
-#    {:cont, []}
-#  end
-#end
+defmodule HL7.MLLPStream do
+  @moduledoc false
+  require Logger
+
+  # ^K - VT (Vertical Tab) - 0x0B
+  @sb "\v"
+  # ^\ - FS (File Separator)
+  @eb <<0x1C>>
+  # ^M - CR (Carriage Return) - 0x0D
+  @cr "\r"
+  @ending @eb <> @cr
+
+  @spec get_prefix() :: String.t()
+  def get_prefix() do
+    @sb
+  end
+
+  @spec get_suffix() :: String.t()
+  def get_suffix() do
+    @ending
+  end
+
+  @spec raw_to_messages(Enumerable.t()) :: Enumerable.t()
+  def raw_to_messages(input_stream) do
+    Stream.chunk_while(input_stream, "", &chunker/2, &after_chunking/1) |> Stream.concat()
+  end
+
+  @spec after_sb(String.t()) :: nil | String.t()
+  defp after_sb(text) do
+    chunks = text |> String.split(@sb, parts: 2)
+
+    case chunks do
+      [_chunk] -> nil
+      [_sb, msg] -> msg
+    end
+  end
+
+  @spec to_list_and_remnant(list()) :: {:cont, list(), String.t()}
+  defp to_list_and_remnant(potential_messages) do
+    [remnant | reverse_msgs] = potential_messages |> Enum.reverse()
+
+    msgs =
+      reverse_msgs
+      |> Enum.map(&after_sb(&1))
+      |> Enum.filter(fn m -> is_binary(m) and m != "" end)
+      |> Enum.reverse()
+
+    {:cont, msgs, remnant}
+  end
+
+  @spec chunker(String.t(), String.t()) :: {:cont, String.t()} | {:cont, list(), String.t()}
+  defp chunker(element, acc) when is_binary(element) do
+    # {:cont, chunk, acc} | {:cont, acc} | {:halt, acc})
+
+    text = acc <> element
+    potential_msg_list = String.split(text, @ending)
+
+    case potential_msg_list do
+      [not_found] -> {:cont, not_found}
+      _ -> potential_msg_list |> to_list_and_remnant
+    end
+  end
+
+  @spec after_chunking(any()) :: {:cont, list()}
+  defp after_chunking(_acc) do
+    {:cont, []}
+  end
+end
