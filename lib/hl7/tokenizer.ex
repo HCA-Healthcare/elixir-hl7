312c9ed0a9d54f54173bacbaa0692612b9b93ac2
LEX5591 <32465602+camilleryr@users.noreply.github.com>
2022-12-09 17:17:42 -0600

    Swap anon fun for macro

--- lib/hl7/tokenizer.ex
+++ lib/hl7/tokenizer.ex
@@ -5,66 +5,72 @@ defmodule HL7.Tokenizer do
   @component 1
   @sub_component 0
 
+  defmacrop copy(should_copy, binary) do
+    quote bind_quoted: [binary: binary, should_copy: should_copy] do
+      case should_copy do
+        true -> :binary.copy(binary)
+        _ -> binary
+      end
+    end
+  end
+
   def tokenize(<<"MSH|^~\\&#", rest::binary>> = text, copy) do
-    tokenize(rest, text, 9, 0, ["^~\\&#", @field, "|", @field, "MSH"], get_copy_fun(copy))
+    tokenize(rest, text, 9, 0, ["^~\\&#", @field, "|", @field, "MSH"], copy)
   end
 
   def tokenize(<<"MSH|^~\\&", rest::binary>> = text, copy) do
-    tokenize(rest, text, 8, 0, ["^~\\&", @field, "|", @field, "MSH"], get_copy_fun(copy))
+    tokenize(rest, text, 8, 0, ["^~\\&", @field, "|", @field, "MSH"], copy)
   end
 
   def tokenize(text, _copy) do
     HL7.DynamicTokenizer.tokenize(text)
   end
 
-  defp tokenize(<<"|", rest::binary>>, original, skip, len, acc, copy_fun) do
-    tokenize_terminator(rest, original, skip, len, acc, @field, copy_fun)
+  defp tokenize(<<"|", rest::binary>>, original, skip, len, acc, copy) do
+    tokenize_terminator(rest, original, skip, len, acc, @field, copy)
   end
 
-  defp tokenize(<<"~", rest::binary>>, original, skip, len, acc, copy_fun) do
-    tokenize_terminator(rest, original, skip, len, acc, @repetition, copy_fun)
+  defp tokenize(<<"~", rest::binary>>, original, skip, len, acc, copy) do
+    tokenize_terminator(rest, original, skip, len, acc, @repetition, copy)
   end
 
-  defp tokenize(<<"^", rest::binary>>, original, skip, len, acc, copy_fun) do
-    tokenize_terminator(rest, original, skip, len, acc, @component, copy_fun)
+  defp tokenize(<<"^", rest::binary>>, original, skip, len, acc, copy) do
+    tokenize_terminator(rest, original, skip, len, acc, @component, copy)
   end
 
-  defp tokenize(<<"&", rest::binary>>, original, skip, len, acc, copy_fun) do
-    tokenize_terminator(rest, original, skip, len, acc, @sub_component, copy_fun)
+  defp tokenize(<<"&", rest::binary>>, original, skip, len, acc, copy) do
+    tokenize_terminator(rest, original, skip, len, acc, @sub_component, copy)
   end
 
-  defp tokenize(<<"\r", rest::binary>>, original, skip, len, acc, copy_fun) do
-    tokenize_terminator(rest, original, skip, len, acc, @carriage_return, copy_fun)
+  defp tokenize(<<"\r", rest::binary>>, original, skip, len, acc, copy) do
+    tokenize_terminator(rest, original, skip, len, acc, @carriage_return, copy)
   end
 
-  defp tokenize(<<"\n", rest::binary>>, original, skip, len, acc, copy_fun) do
-    tokenize_terminator(rest, original, skip, len, acc, @carriage_return, copy_fun)
+  defp tokenize(<<"\n", rest::binary>>, original, skip, len, acc, copy) do
+    tokenize_terminator(rest, original, skip, len, acc, @carriage_return, copy)
   end
 
-  defp tokenize(<<_char::binary-size(1), rest::binary>>, original, skip, len, acc, copy_fun) do
-    tokenize(rest, original, skip, len + 1, acc, copy_fun)
+  defp tokenize(<<_char::binary-size(1), rest::binary>>, original, skip, len, acc, copy) do
+    tokenize(rest, original, skip, len + 1, acc, copy)
   end
 
-  defp tokenize("", _original, _skip, 0, acc, _copy_fun) do
+  defp tokenize("", _original, _skip, 0, acc, _copy) do
     Enum.reverse(acc)
   end
 
-  defp tokenize("", original, skip, len, acc, copy_fun) do
-    string = copy_fun.(binary_part(original, skip, len))
+  defp tokenize("", original, skip, len, acc, copy) do
+    string = copy(copy, binary_part(original, skip, len))
     Enum.reverse([string | acc])
   end
 
-  defp tokenize_terminator(text, original, skip, 0, acc, terminator, copy_fun) do
-    tokenize(text, original, skip + 1, 0, [terminator | acc], copy_fun)
+  defp tokenize_terminator(text, original, skip, 0, acc, terminator, copy) do
+    tokenize(text, original, skip + 1, 0, [terminator | acc], copy)
   end
 
-  defp tokenize_terminator(text, original, skip, len, acc, terminator, copy_fun) do
-    string = copy_fun.(binary_part(original, skip, len))
-    tokenize(text, original, skip + len + 1, 0, [terminator, string | acc], copy_fun)
+  defp tokenize_terminator(text, original, skip, len, acc, terminator, copy) do
+    string = copy(copy, binary_part(original, skip, len))
+    tokenize(text, original, skip + len + 1, 0, [terminator, string | acc], copy)
   end
-
-  defp get_copy_fun(true), do: &:binary.copy/1
-  defp get_copy_fun(false), do: &Function.identity/1
 
   @compile {:inline, tokenize: 6, tokenize_terminator: 7}
 end
