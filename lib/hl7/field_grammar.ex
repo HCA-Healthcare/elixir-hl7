dd340045644edf8e8145ff331a42a2acafa44c72
Flora Petterson <flora.petterson@gmail.com>
2024-09-01 09:53:58 -0500

    field grammar

--- lib/hl7/field_grammar.ex
+++ lib/hl7/field_grammar.ex
@@ -1,5 +1,9 @@
 defmodule HL7.FieldGrammar do
-  @deprecated "Use HL7.Path.new/1 instead"
+  @moduledoc false
+
+  @doc """
+  Use HL7.Path.new/1 instead
+  """
   def to_indices(schema) do
     HL7.Path.new(schema)
   end
