34403edbadf3578da04933062559e5c1e867e5c4
Flora Petterson <flora.petterson@gmail.com>
2024-09-01 14:40:06 -0500

    need to fix doctest

--- lib/hl7/examples.ex
+++ lib/hl7/examples.ex
@@ -7,6 +7,10 @@ defmodule HL7.Examples do
   Returns a sample HL7 string from [Wikipedia's HL7 article](https://en.wikipedia.org/wiki/Health_Level_7#Version_2_messaging).
 
   The HL7 version of the message defaults to 2.5, but can be overidden.
+
+  ## Examples
+      iex> HL7.Examples.wikipedia_sample_hl7() |> to_string() |> String.replace("~\\&", "~&")
+      "MSH|^~&|MegaReg|XYZHospC|SuperOE|XYZImgCtr|20060529090131-0500||ADT^A01^ADT_A01|01052901|P|2.5\rEVN||200605290901||||200605290900\rPID|||56782445^^^UAReg^PI||KLEINSAMPLE^BARRY^Q^JR||19620910|M||2028-9^^HL70005^RA99113^^XYZ|260 GOODWIN CREST DRIVE^^BIRMINGHAM^AL^35209^^M~NICKELL’S PICKLES^10000 W 100TH AVE^BIRMINGHAM^AL^35200^^O|||||||0105I30001^^^99DEF^AN\rPV1||I|W^389^1^UABH^^^^3||||12345^MORGAN^REX^J^^^MD^0010^UAMC^L||67890^GRAINGER^LUCY^X^^^MD^0010^UAMC^L|MED|||||A0||13579^POTTER^SHERMAN^T^^^MD^0010^UAMC^L|||||||||||||||||||||||||||200605290900\rOBX|1|N^K&M|^Body Height||1.80|m^Meter^ISO+|||||F\rOBX|2|NM|^Body Weight||79|kg^Kilogram^ISO+|||||F\rAL1|1||^ASPIRIN\rDG1|1||786.50^CHEST PAIN, UNSPECIFIED^I9|||A\r"
   """
 
   @spec wikipedia_sample_hl7(String.t()) :: String.t()
