47db3a4beb7c7314683b5180234c2f963c07457e
Flora Petterson <flora.petterson@gmail.com>
2024-10-10 18:22:48 -0500

    update field grammar

--- lib/hl7/field_grammar.ex
+++ lib/hl7/field_grammar.ex
@@ -1,6 +1,8 @@
 defmodule HL7.FieldGrammar do
-  @moduledoc deprecated: "Use `HL7.Path.new/1` instead"
+  @moduledoc deprecated: "Use `HL7` instead"
+  @moduledoc "Represents an HL7 path."
 
+  @doc deprecated: "Use `HL7.Path.new/1` instead"
   def to_indices(schema) do
     HL7.Path.new(schema)
   end
